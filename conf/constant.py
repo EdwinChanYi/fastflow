8c88d9ee164053ffe9fbe8f1d5d83d15246288fe
ediwn <56284823@qq.com>
2019-02-10 16:07:35 +0800

    const

--- conf/constant.py
+++ conf/constant.py
@@ -10,13 +10,6 @@ REDIS_SHOP_DETAIL_HOST = 'shop_detail_host_%s'      # 商店根据域名获取�
 REDIS_USER_ID = 'user_detail_id_%s'                 # 用户根据id获取详情
 
 # mysql key
-MYSQL_JIGEYI_DB = 'jigeyi'
-# 商城数据库表名
-MYSQL_SHOPPING_MALL_MATERIAL_TABLE = 'sm_material_kind_%s'
-MYSQL_SHOPPING_MALL_RECIPE_TABLE = 'sm_recipe_%s'
-MYSQL_SHOP_TABLE = 'shop'
-MYSQL_SHOPPING_MALL_KIND_TABLE = 'sm_material_kind_info'
-MYSQL_SHOPPING_MALL_MATERIAL_RELATE_RECIPE_TABLE = 'sm_material_relate_recipe'
-MYSQL_SHOPPING_MALL_RECIPE_DAILY_TABLE = 'sm_recipe_daily'
-MYSQL_SHOPPING_MALL_SHOP_CAR_TABLE = 'sm_shop_car'
-MYSQL_ORDER_USER_SHOPPING_ORDER_TABLE = 'order_user_shopping_order'
+
+DAILY_TASK_TABLE = "daily_task"
+DAILY_TASK_KEY = {"id", "uid", "content", "summary", "state", "create_time", "aim"}
